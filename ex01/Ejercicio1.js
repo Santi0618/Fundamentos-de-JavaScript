25a577b59933123edf7387c3acfb9d662db159a7
Samuel Serna Muñoz <samuelserna1906@gmail.com>
2026-05-28 20:14:28 -0500

    subiendo

---
+++ ex01/Ejercicio1.js
@@ -0,0 +1,9 @@
+/* 
+Esto sirve para realizar comentarios teoricos, es decir, para explicar el codigo, o para escribir cualquier texto que no se ejecute como codigo.
+Los comentarios pueden ser utiles para explicar el proposito de ciertas partes del codigo, para dejar notas para otros desarrolladores, o para recordar algo importante sobre el codigo en el futuro.
+ */
+
+// Esto es un comentario de una sola linea, se utiliza para escribir comentarios breves o para comentar una sola linea de codigo.
+
+document.write("Hola Mundo"); 
+console.log("Hola desde la consola");
