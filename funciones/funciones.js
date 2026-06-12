b4cf6ce18b1213d49fa91429d2f3aac1a245e817
Samuel Serna Muñoz <samuelserna1906@gmail.com>
2026-06-11 21:18:07 -0500

    Funciones

---
+++ funciones/funciones.js
@@ -0,0 +1,14 @@
+function saludar (){
+    console.log("Bienvenidos a un saludo de funcion")
+}
+
+saludar();
+
+
+
+function suma (x, y){
+    return x + y;
+
+}
+let resultado = suma (5, 3); // La variable resultado esta invocando la funcion suma
+console.log(resultado); // imprimie 8 
